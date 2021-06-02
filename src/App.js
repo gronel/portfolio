d50f331894dd90a33f9e6f6011b1bdc8e245e92c
gronel <ronelgonzales5@gmail.com>
2021-06-02 15:56:40 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
       </a>
 
       <ul className="nav nav-pills">
-        <li className="nav-item"><a href="#" className="nav-link active" aria-current="page">Home</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">Features</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">Pricing</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">FAQs</a></li>
-        <li className="nav-item"><a href="#" className="nav-link">About</a></li>
+        <li className="nav-item"><a href="/" className="nav-link active" aria-current="page">Home</a></li>
+        <li className="nav-item"><a href="/" className="nav-link">Features</a></li>
+        <li className="nav-item"><a href="/" className="nav-link">Pricing</a></li>
+        <li className="nav-item"><a href="/" className="nav-link">FAQs</a></li>
+        <li className="nav-item"><a href="/" className="nav-link">About</a></li>
       </ul>
     </header>
   </div>
